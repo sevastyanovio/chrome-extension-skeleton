93a81146214de30835459d4be8c1b29db9cd9fe3
Roman Sevastyanov <avex6996@gmail.com>
2017-02-12 23:12:07 +0700

    first version

---
+++ brunch-config.js
@@ -0,0 +1,20 @@
+// See http://brunch.io for documentation.
+exports.files = {
+  javascripts: {
+    joinTo: {
+      'vendor.js': /^(?!app)/, // Files that are not in `app` dir.
+      'app.js': /^app/
+    }
+  },
+  stylesheets: {
+    joinTo: 'app.css'
+  }
+};
+
+exports.paths = {
+  public: 'extension/build/'
+};
+
+exports.plugins = {
+  babel: {presets: ['latest']}
+};
